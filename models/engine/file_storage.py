c1ff80ac8463301166ad0313c61e0e9836ff49dc
LaurelleJinelle <156344054+LaurelleJinelle@users.noreply.github.com>
2024-09-09 07:27:29 -0700

    file_storage.py update User, amenity, city, place, reviewand state files created and updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,3 +36,40 @@ class FileStorage:
                     class_name = key.split('.')[0]  # Extract class name from key
                     # Assuming dynamic class instantiation using eval
                     self.__objects[key] = eval(f"{class_name}(**value)")
+
+    def reload(self):
+
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.state import State
+        from models.amenity import Amenity
+        from models.review import Review
+
+        # This is the dictionary we will be loading from the JSON file
+        file_name = FileStorage.__file_path
+        # We need to check if the file exists before we try to open it but we
+        #   don't do anything if it doesn't exist
+        if exists(file_name):
+            # Here we open the file but since we are only reading from it,
+            # We don't need to specify a mode or encode(cuz it's the default)
+            with open(file_name) as f:
+                obj_dict = load(f)
+                # print(obj_dict)
+        # At this point, we're able to print the JSON file but are still
+        # in a dictionary form and need to be converted to object form
+
+            for value in obj_dict.values():
+                # This gets the class name of the object & the eval() function
+                # makes sure the class type is "type" and not a string
+                class_name = eval(value["__class__"])
+                del value["__class__"]
+                # Now we want to create a new object from the dictionary values
+                # by passing them as keyword arguments to BaseModel, user...etc
+                # Cuz both in the classes & here objects are changed to to_dict
+                #   so when passed through ** we get new objects
+                obj = class_name(**value)
+                # Here we call on the new method from above because we want to
+                # pass each newly iterated object to __objects
+                self.new(obj)
